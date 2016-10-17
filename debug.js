1d57a6866e29533a7cae0722eeb69f4aceced1f7
maumasi <liu.maumasi@gmail.com>
2016-10-17 19:19:14 -0400

    Added the ability to logs errors tot the log file

--- debug.js
+++ debug.js
@@ -3,15 +3,19 @@ const fs = require('fs');
 const colors = require('colors');
 
 // Debug Module, if DEBUG=true nodemon src/server.js console.log will be ON
-module.exports = function debugWash(date, msg, obj) {
+module.exports = function debugWash(err, msg, obj) {
+  const date = new Date();
   if (process.env.DEBUG) {
-    // Console logging data
-    console.log(date.inverse, msg.bgYellow, obj);
+    if (err) {
+      console.log(date.inverse, msg.bgYellow, err);
+      fs.appendFile('./logs/debugs.log', `${date} ${msg} ${err} \n`);
+    } else {
+      // Console logging data
+      console.log(date.inverse, msg.bgYellow, obj);
 
-
-// Sending Console.log Messages to a Logs/debugs.log file
-    JSON.stringify(obj);
-    fs.appendFile('./logs/debugs.log', `${date} ${msg} ${JSON.stringify(obj)} \n`
-  );
-  }
-};
+      // Sending Console.log Messages to a Logs/debugs.log file
+      JSON.stringify(obj);
+      fs.appendFile('./logs/debugs.log', `${date} ${msg} ${JSON.stringify(obj)} \n`);
+    } // child if
+  } // parent if
+}; // exports
